33c308d82c12e9b365d62bf4e94879918b07da91
AditiRgithub <aditirao130804@gmail.com>
2026-07-18 21:33:30 +0530

    Update Django settings for Vercel

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -88,16 +88,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "bookmyseat.wsgi.application"
 
-# Database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        "postgresql://django_bookmyseat_a2fm_user:X5xjULEJLltuxYudIj7MkpKEGe56KT4C@dpg-d9dpbuf41pts73dkq7rg-a.virginia-postgres.render.com/django_bookmyseat_a2fm"
+    )
 }
-
-DATABASES['default']=dj_database_url.parse('postgresql://django_bookmyseat_a2fm_user:X5xjULEJLltuxYudIj7MkpKEGe56KT4C@dpg-d9dpbuf41pts73dkq7rg-a.virginia-postgres.render.com/django_bookmyseat_a2fm')
-
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
